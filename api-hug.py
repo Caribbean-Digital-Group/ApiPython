e0da66bbc253157b34490b3d3bd6ef0fa5d71413
Jose Gomez <Chkm64.goma@gmail.com>
2020-10-22 16:40:17 -0500

    Primer ApiRest
    Este es un ejemplo sencillo obtenido de la pagina:
    https://laesporadelhongo.com/wp-content/cache/page_enhanced/laesporadelhongo.com/crear-apis-rest-con-python-y-hug/_index.html_gzip

--- api-hug.py
+++ api-hug.py
@@ -1,1 +1,21 @@
-print("Mi primer Api en Python")
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Segundo método: acceso http"""
+import datetime
+import hug
+
+
+@hug.get(examples="name=Jhon Doe&amp;age=30")
+@hug.local()
+def say_hello(name: hug.types.text, age: hug.types.number, hug_timer=3):
+    """Decimos hola al usuario y calculamos su año de nacimiento"""
+    year_of_birth = datetime.datetime.now().year - age
+    return {
+        'message': "Hola {0}, naciste el año {1}".format(name, year_of_birth),
+        'took': float(hug_timer)
+    }
+
+
+if __name__ == '__main__':
+    print(say_hello("Jose del Carmen", 29))
